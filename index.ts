1da581d184d14e5ff46d0a2081a38dce56ea534b
ExternalAddress4401 <external_address@hotmail.com>
2023-07-31 20:42:57 -0700

    Added liveopsdeeplink

--- index.ts
+++ index.ts
@@ -1,6 +1,21 @@
 import fs from "fs";
 import ProtobufReader from "./ProtobufReader";
 
+const cmsConfigs = [
+  "GameConfig",
+  "LangConfig",
+  "AssetsPatchConfig",
+  "AudioConfig",
+  "NewsFeed",
+  "ScalingConfig",
+  "NotificationsConfig",
+  "FontFallbackConfig",
+  "LiveOpsBundleConfig",
+  "LiveOpsEventConfig",
+  "LiveOpsDeepLinkRewardConfig",
+  "SongConfig",
+];
+
 const file = fs.readFileSync("./cms/AssetsPatchConfig.bytes");
 
 const reader = new ProtobufReader(file);
@@ -8,6 +23,8 @@ const reader = new ProtobufReader(file);
 const proto = JSON.parse(
   fs.readFileSync("./protos/AssetsPatchConfig.json").toString()
 );
+
+export { ProtobufReader };
 
 const raw = reader.process(proto);
 console.log(raw);
