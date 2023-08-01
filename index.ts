92f8beda9d41501bace060cfa2c13ab40fbcbe73
ExternalAddress4401 <external_address@hotmail.com>
2023-07-31 20:23:53 -0700

    Added LiveOpsBundleConfig

--- index.ts
+++ index.ts
@@ -1,13 +1,15 @@
 import fs from "fs";
 import ProtobufReader from "./ProtobufReader";
 
-const file = fs.readFileSync("./cms/NotificationsConfig.bytes");
+const file = fs.readFileSync("./cms/LiveOpsBundleConfig.bytes");
 
 const reader = new ProtobufReader(file);
 
 const proto = JSON.parse(
-  fs.readFileSync("./protos/NotificationsConfig.json").toString()
+  fs.readFileSync("./protos/LiveOpsBundleConfig.json").toString()
 );
+
+console.log(proto);
 
 const raw = reader.process(proto);
 console.log(raw);
